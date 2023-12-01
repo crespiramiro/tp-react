155346143ab9ab145136856663845d1ebcc1d347
crespiramiro <crespiramiro@outlook.com>
2023-12-01 11:54:52 -0300

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   src/app/layout.jsx
    	modified:   src/components/HeroImg.jsx
    	modified:   src/components/productos/Products.jsx
    	modified:   src/components/productos/ProductsContainer.jsx
    	modified:   tailwind.config.js

--- src/components/productos/ProductsContainer.jsx
+++ src/components/productos/ProductsContainer.jsx
@@ -3,7 +3,7 @@ import Products from "./Products";
 function ProductsContainer() {
     return (  
         <section id="productos" className="h-screen w-full" >
-                    <h2 className=" text-3xl text-center py-8 font-bold " >Nuestros <span className="text-green-800" > Productos</span></h2>
+                    <h2 className=" text-2xl lg:text-4xl text-center py-8 font-bold " >Nuestros <span className="text-green-800" > Productos</span></h2>
                     <div className="grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mx-4 gap-y-8 " >
                         <Products  img= 'productos/taza.avif' title='Taza de Ceramica' price='$4599' oldPrice='5799' alt="taza de ceramica"  />
                         <Products  img= 'productos/taza.avif' title='Taza de Ceramica' price='$4599' oldPrice='5799' alt="taza de ceramica"  />
