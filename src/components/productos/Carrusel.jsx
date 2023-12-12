364686d3b719dc6375e0c5db556c1a80c366d7cf
Zahira-Rejala <rejalazahira@gmail.com>
2023-12-12 20:12:24 -0300

    carrusel terminado

--- src/components/productos/Carrusel.jsx
+++ src/components/productos/Carrusel.jsx
@@ -1,41 +1,65 @@
 'use client'
-import React, {useEffect, useRef, useState} from "react";
-import { data } from "../../assets/data.js";
+import Image from "next/image"
+import "./stylescarousel.css";
+import React, { Component } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import { Carousel } from 'react-responsive-carousel';
+import { execOnce } from "next/dist/shared/lib/utils";
+import { Dancing_Script } from "next/font/google";
+const dancing = Dancing_Script({subsets: ['latin']})
 
-export const Carrusel = () => {
-    // const listRef = useRef 
-    const [currentIndex, setCurrentIndex] = useState (0)
 
-useEffect (() =>{
-//  const listNode = listRef.current;
-//  const imgNode = list.listNode.querySelectorAll("li > img") [currentIndex]
 
-// if (imgNode) {
-//     imgNode.scrollIntoView ({
-//         behavior: "smooth"
-//     })
-// }
-
-}, [currentIndex])
-
-    return(
-        <div className=" bg-black h-[280px] w-[250px] m-0">
-        <div className="relative h-full">
-<div className=" h-full w-full rounded-s-2xl border-solid border-blue-500 truncate4">
-    shdbads
-{/* <ul ref={listRef}>
-{
-    data.map((item) =>{
-        return <li key={item.id}>
-            <img src={item.imgUrl} width={500} height={280} />
-        </li>
-    })
-}
-</ul> */}
+import img1 from "../../assets/imagenes/idea1.webp"
+import img2 from "../../assets/imagenes/idea 2.webp"
+import img3 from "../../assets/imagenes/idea 3.webp"
+import img4 from "../../assets/imagenes/idea 4.webp"
+import img5 from "../../assets/imagenes/idea 5.webp"
+function Carrusel() {
+    return (
+       <div>
+        <div> 
+            <h1 className={`text-3xl ${dancing.className} text-center underline`} >Ideas con ceramica</h1>
+            <h2 className="text-xl text-center font-serif">Te dejamos unas ideas para decorar tu casa con estilo.</h2>
+        </div>
+<Carousel className="mainslide">
+<div>
+    <Image src={img1} 
+    alt={"Idea 1"}
+    layout="responsive" />
 </div>
-        </div>
-        </div>
+<div>
+<Image src={img2} 
+    height={50} 
+    width={50}
+    alt={"Idea 2"}
+    layout="responsive" />
+</div>
+<div>
+<Image src={img3} 
+    height={50} 
+    width={50}
+    alt={"Idea 3"}
+    layout="responsive" />
+</div>
+<div>
+<Image src={img4} 
+    height={50} 
+    width={50}
+    alt={"Idea 4"}
+    layout="responsive" />
+</div>
+<div>
+<Image src={img5} 
+    height={50} 
+    width={50}
+    alt={"Idea 5"}
+    layout="responsive" />
+</div>
+</Carousel>
+</div>
     )
+    
 }
 
 export default Carrusel;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
-      
+      fontFamily: {
+        "dancingscript": ['Dancing Script', 'cursive']
+      }
     },
   },
   plugins: [],
