fcd514cc10a4e6b3d03bfb283a4c1ccb84dffeec
crespiramiro <crespiramiro@outlook.com>
2023-12-23 22:59:04 -0300

    backend commit

--- src/components/HeroImg.jsx
+++ src/components/HeroImg.jsx
@@ -3,7 +3,7 @@ const dancing = Dancing_Script({subsets: ['latin']})
 
 function HeroImg() {
     return (
-        <div  className="w-full flex justify-start place-items-center h-[30rem] bg-cover " style={{backgroundImage: "url('hero-ceramica.avif' ) "}} >
+        <div  className="w-full flex justify-start place-items-center h-[30rem] bg-cover " style={{backgroundImage: "url('hero-ceramica.webp' ) "}} >
             <h2 className={` text-green-300 w-1/3 text-center font-semibold  text-5xl ${dancing.className} `} >Donde el arte cobra vida en cada pieza</h2>
         </div>
       );
