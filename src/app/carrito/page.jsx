e416192e3d3da78e10b3abe07613594370734db7
Alejandro Roman <alejandroroman88@gmail.com>
2024-01-09 12:59:11 -0300

    inicio-carrito

--- src/app/carrito/page.jsx
+++ src/app/carrito/page.jsx
@@ -1,7 +1,16 @@
+// import styles from "@/styles/Carrito.module.css";
 export default function Home() {
   return (
-    <main className="h-screen w-full overflow-x-hidden bg-green-100">
-      <p>hola</p>
+    <main
+    // className={
+    //   "${styles.contenido} contenedor h-screen w-full overflow-x-hidden bg-green-100"
+    // }
+    >
+      <div className="flex flex-row gap-4 p-4 border-4">
+        <div className="col-span-1">Productos</div>
+        <div className="col-span-2">Precios</div>
+        <div className="col-span-2">Cantidad</div>
+      </div>
     </main>
   );
 }
