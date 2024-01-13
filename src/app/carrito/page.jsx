71b6df9621cb0a06032dddb4f27a125e1df01c58
Alejandro Roman <alejandroroman88@gmail.com>
2024-01-13 00:50:29 -0300

    Solucion error useReducer

--- src/app/carrito/page.jsx
+++ src/app/carrito/page.jsx
@@ -1,48 +1,31 @@
-// import styles from "@/styles/Carrito.module.css";
-// export default function Home() {
-//   return (
-//     <main
-//     // className={
-//     //   "${styles.contenido} contenedor h-screen w-full overflow-x-hidden bg-green-100"
-//     // }
-//     >
-//       <div className="flex flex-row gap-4 p-4 border-solid border-2 border-black">
-//         <div className="col-span-1 border-solid border-4 border-black">
-//           Productos
-//         </div>
-//         <div className="col-span-2 border-solid border-4 border-black">
-//           Cantidad
-//         </div>
-//         <div className="col-span-3 border-solid border-4 border-black">
-//           Precio
-//         </div>
-//         <div className="col-span-4 border-solid border-4 border-black">
-//           Total
-//         </div>
-//       </div>
-//     </main>
-//   );
-// }
-
+"use client";
 import { useReducer } from "react";
 import {
   shoppingInitialState,
   shoppingReducer,
 } from "@/reducers/shoppingReducer";
+import Navbar from "@/components/Navbar";
 
 const ShoppingCart = () => {
   const [state, dispatch] = useReducer(shoppingReducer, shoppingInitialState);
   const { products, cart } = state;
 
+  const addToCart = () => {};
+  const delToCart = () => {};
+  const clearCart = () => {};
+
   return (
-    <div>
-      <h2>Carrito de Compras</h2>
-      <h3>Productos</h3>
-      <article className="box"></article>
-      <h3>Carrito</h3>
-      <article className="box"></article>
-    </div>
+    <main className="h-screen w-full overflow-x-hidden bg-green-100">
+      <Navbar />
+      <div>
+        <h2>Carrito de Compras</h2>
+        <h3>Productos</h3>
+        <article className="box"></article>
+        <h3>Carrito</h3>
+        <article className="box"></article>
+      </div>
+    </main>
   );
 };
 
-export default page;
+export default ShoppingCart;
--- src/components/NewProducts.jsx
+++ src/components/NewProducts.jsx
@@ -1,45 +1,55 @@
-'use client'
-import { useEffect, useState } from "react"
-import axios from "axios"
+"use client";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function NewProducts(){
+export default function NewProducts() {
+  const [productos, setProductos] = useState([]);
 
-    const [productos, setProductos] = useState([]);
-    
-    const getData = async () => {
+  const getData = async () => {
+    const res = await axios.get("http://localhost:5050/products");
+    const products = await res.data;
+    setProductos(products);
+    console.log(products);
+  };
 
-        const res = await axios.get('http://localhost:5050/products');
-        const products = await res.data;
-        setProductos(products)
-        console.log(products);
+  useEffect(() => {
+    getData();
+  }, []);
 
-    }
-
-    useEffect(()=>{
-        getData()
-    },[])
-
-    return (
-
-        <section id="productos" className="h-1/2 w-full" >
-        <h2 className=" text-2xl lg:text-4xl text-center py-8 font-bold " >Nuestros <span className="text-green-800" > Productos</span></h2>
-        <div className="grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mx-4 gap-y-8 " >
-            {productos.map((producto => (
-                <div key={producto.id} className="h-auto flex flex-col gap-y-1 w-auto rounded-lg shadow-lg justify-center items-center mx-4 " >
-                <img src={producto.img} alt={producto.name} className="rounded-md pb-1 h-90 object-cover"loading="lazy"/>
-                <h2 className="text-center lg:text-2xl font-semibold " >{producto.name}</h2>
-                <div className="prices p-1 flex flex-row gap-x-2 justify-center ">
-                    <h3 className="text-green-600 lg:text-xl " >{producto.price}</h3>
-                    <h3 className="line-through opacity-50 lg:text-lg " >{producto.oldprice}</h3>
-                </div>
-                <button className="bg-green-700 p-3 mb-4 rounded-md text-center " >
-                    <p className="text-white font-semibold text-center lg:text-lg " >Agregar al Carrito</p>
-                </button>
-        </div>
-            ) ))}
-        </div>
-</section>
-
-    )
-
+  return (
+    <section id="productos" className="h-1/2 w-full">
+      <h2 className=" text-2xl lg:text-4xl text-center py-8 font-bold ">
+        Nuestros <span className="text-green-800"> Productos</span>
+      </h2>
+      <div className="grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mx-4 gap-y-8 ">
+        {productos.map((producto) => (
+          <div
+            key={producto.id}
+            className="h-auto flex flex-col gap-y-1 w-auto rounded-lg shadow-lg justify-center items-center mx-4 "
+          >
+            <img
+              src={producto.img}
+              alt={producto.name}
+              className="rounded-md pb-1 h-90 object-cover"
+              loading="lazy"
+            />
+            <h2 className="text-center lg:text-2xl font-semibold ">
+              {producto.name}
+            </h2>
+            <div className="prices p-1 flex flex-row gap-x-2 justify-center ">
+              <h3 className="text-green-600 lg:text-xl ">{producto.price}</h3>
+              <h3 className="line-through opacity-50 lg:text-lg ">
+                {producto.oldprice}
+              </h3>
+            </div>
+            <button className="bg-green-700 p-3 mb-4 rounded-md text-center ">
+              <p className="text-white font-semibold text-center lg:text-lg ">
+                Agregar al Carrito
+              </p>
+            </button>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
