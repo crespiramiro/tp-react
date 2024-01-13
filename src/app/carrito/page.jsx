889ea6675f2aa87fec0023b41dee9870afb12b43
Alejandro Roman <alejandroroman88@gmail.com>
2024-01-13 03:19:05 -0300

    avance-carrito

--- src/app/carrito/page.jsx
+++ src/app/carrito/page.jsx
@@ -5,14 +5,30 @@ import {
   shoppingReducer,
 } from "@/reducers/shoppingReducer";
 import Navbar from "@/components/Navbar";
+import ProductItem from "@/components/ProductItem";
+import "@/styles/ShoppingCartStyles.css";
+import CartItem from "@/components/CartItem";
+import { TYPES } from "@/actions/shoppingAction";
 
 const ShoppingCart = () => {
   const [state, dispatch] = useReducer(shoppingReducer, shoppingInitialState);
   const { products, cart } = state;
 
-  const addToCart = () => {};
-  const delToCart = () => {};
-  const clearCart = () => {};
+  const addToCart = (id) => {
+    // console.log(id);
+    dispatch({ type: TYPES.ADD_TO_CART, payload: id });
+  };
+  const delFromCart = (id, all = false) => {
+    // console.log(id, all);
+    if (all) {
+      dispatch({ type: TYPES.REMOVE_ALL_FROM_CART, payload: id });
+    } else {
+      dispatch({ type: TYPES.REMOVE_ONE_FROM_CART, payload: id });
+    }
+  };
+  const clearCart = () => {
+    dispatch({ type: TYPES.CLEAR_CART });
+  };
 
   return (
     <main className="h-screen w-full overflow-x-hidden bg-green-100">
@@ -20,9 +36,22 @@ const ShoppingCart = () => {
       <div>
         <h2>Carrito de Compras</h2>
         <h3>Productos</h3>
-        <article className="box"></article>
+        <article className="box grid-responsive">
+          {products.map((product) => (
+            <ProductItem
+              key={product.id}
+              data={product}
+              addToCart={addToCart}
+            />
+          ))}
+        </article>
         <h3>Carrito</h3>
-        <article className="box"></article>
+        <article className="box">
+          <button onClick={clearCart}>Limpiar Carrito</button>
+          {cart.map((item, index) => (
+            <CartItem key={index} data={item} delFromCart={delFromCart} />
+          ))}
+        </article>
       </div>
     </main>
   );
